d8c0100a1037738e5325280aa6d3d17edffed2eb
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-10-18 13:44:24 +0530

    Add:test case to send i am in sad mood message in constructor return happy

--- test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
+++ test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -13,5 +13,11 @@ public class MoodAnalyserTest {
         String actualResult = moodanalyser.analyseMood();
         Assert.assertEquals("Sad", actualResult);
     }
+	@Test
+    public void givenMessageWhenProperShouldReturnHappy() {
+        MoodAnalyser moodanalyser = new MoodAnalyser("I am in happy Mood");
+        String actualResult = moodanalyser.analyseMood();
+        Assert.assertEquals("Happy", actualResult);
+    }
 	
 }
